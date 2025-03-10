7b7dc129927303f73a6b2b175fb9e2cd79af4a2e
Brett Chaldecott <brett.chaldecott@gmail.com>
2025-03-10 15:59:03 +0200

    feat: added the ruby auth controller

--- lib/kinde_sdk.rb
+++ lib/kinde_sdk.rb
@@ -2,6 +2,7 @@ require "kinde_sdk/version"
 require "kinde_sdk/configuration"
 require "kinde_sdk/client/feature_flags"
 require "kinde_sdk/client/permissions"
+require "kinde_sdk/controllers/auth_controller"
 require "kinde_sdk/client"
 
 require 'securerandom'
