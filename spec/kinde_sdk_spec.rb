7ac380142fcb54dc5d969b9531ba327d40de31be
Brett Chaldecott <brett.chaldecott@gmail.com>
2025-03-25 11:49:18 +0200

    fix: update the tests

--- spec/kinde_sdk_spec.rb
+++ spec/kinde_sdk_spec.rb
@@ -92,9 +92,10 @@ describe KindeSdk do
       stub_request(:get, "#{domain}/.well-known/jwks.json")
         .with(
           headers: {
-            'User-Agent' => "Kinde-SDK: Ruby/#{KindeSdk::VERSION}"
-          }
-        )
+       	  'Accept'=>'*/*',
+       	  'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+       	  'User-Agent'=>'Ruby'
+           })
         .to_return(
           status: 200,
           body: jwks_hash.to_json,
