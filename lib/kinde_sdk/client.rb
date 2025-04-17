a9d87b86bb950d1e6f22094ec82508d8ebb27f68
Brett Chaldecott <brett.chaldecott@gmail.com>
2025-04-17 10:17:44 +0200

    fix: corrected the logic blocking token refresh

--- lib/kinde_sdk/client.rb
+++ lib/kinde_sdk/client.rb
@@ -49,6 +49,9 @@ module KindeSdk
     private
 
     def set_hash_related_data(tokens_hash)
+      # Validate tokens before setting them
+      KindeSdk.validate_jwt_token(tokens_hash)
+      
       @tokens_hash = tokens_hash.transform_keys(&:to_sym)
       @bearer_token = @tokens_hash[:access_token]
       @expires_at = @tokens_hash[:expires_at]
